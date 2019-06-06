7ad444f8091178d1038db55d3027dc4482766daa
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2019-06-06 16:27:01 +0200

    Create react app and adding function components

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Form from './Form';
+import Itemlist from './ItemList';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Liste de commissions</h3>
+                <div>
+                    < Form />
+                    < Itemlist />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App; 
